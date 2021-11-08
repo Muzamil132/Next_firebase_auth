e370b0192821b402ae33e54bed56574fac362623
Muzamil132 <muzamil073@gmail.com>
2021-11-08 01:17:28 -0800

    changed config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   PORT_URL:"http://localhost:3000"
+
+  ,  async rewrites() {
+        return [
+          {
+            source: 'https://next-firebase-auth1-qifsbz3ny-muzamil132.vercel.app/api:path*',
+            destination: 'https://next-firebase-auth1-qifsbz3ny-muzamil132.vercel.app/:path*',
+          },
+        ]
+      },
 }
